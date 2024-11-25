a02650b4659fb560336662b290da871b1d7c1e07
iannn05 <101185464+iannn05@users.noreply.github.com>
2024-11-25 12:23:36 -0300

    Update user.services.ts

--- src/app/services/user.services.ts
+++ src/app/services/user.services.ts
@@ -18,7 +18,7 @@ export class UserService {
     })
   };
 
-  private BASE_URL = 'http://localhost:3000/usuario';
+  private BASE_URL = 'http://localhost:8081/usuario';
 
   constructor(private http: HttpClient) { }
 
