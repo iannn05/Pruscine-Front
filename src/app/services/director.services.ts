1d05a655c24fab7f0941248b1bf14f12329f8db0
iannn05 <101185464+iannn05@users.noreply.github.com>
2024-11-28 13:51:14 -0300

    Update director.services.ts

--- src/app/services/director.services.ts
+++ src/app/services/director.services.ts
@@ -22,7 +22,7 @@ export class DirectorService {
     };
   }
 
-  private BASE_URL = 'http://192.168.1.11:3000/director';
+  private BASE_URL = 'http://172.18.0.1:3000/director';
 
   constructor(private http: HttpClient) { }
 
