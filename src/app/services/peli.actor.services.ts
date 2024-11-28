7b38a87633de08f97ad8078bc2430a573330057e
iannn05 <101185464+iannn05@users.noreply.github.com>
2024-11-28 13:51:55 -0300

    Update peli.actor.services.ts

--- src/app/services/peli.actor.services.ts
+++ src/app/services/peli.actor.services.ts
@@ -18,7 +18,7 @@ export class PeliActorService {
     })
   };
 
-  private BASE_URL = 'http://192.168.1.11:3000/peliculaActor';
+  private BASE_URL = 'http://172.18.0.1:3000/peliculaActor';
 
   constructor(private http: HttpClient) { }
 
